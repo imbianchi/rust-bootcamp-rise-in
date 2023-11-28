b6f63c1e9321e5a5c61de5e06c2abd0266568b0f
Joao Paulo (JP) <joaobianchi@live.com>
2023-11-28 16:20:46 -0300

    :construction: module 1 - ownership & string slices

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-pub mod ownership;
+// method that concanate two string
+// @params:
+// s1 -> string slice
+// s2 -> string slice
+// returns String
+pub fn concatenate_strings(s1: &str, s2: &str) -> String {
+    // creates new string to store the result
+    let mut result = String::new();
+
+    // append first and second paramenter to the result string
+    result.push_str(s1);
+    result.push_str(s2);
+
+    // return the result String concatenated
+    result
+}
 
 fn main() {
-    //mod 1 - ownership
+    // mod 1 - ownership
+    // initialize two string slices
     let string1 = "Rust ";
     let string2 = "rules!";
 
-    println!("{}", ownership::concatenate_strings(string1, string2));
+    // call method to concatenate both and return the result
+    println!("{}", concatenate_strings(string1, string2));
 }
--- src/ownership/mod.rs
+++
@@ -1,8 +0,0 @@
-pub fn concatenate_strings(s1: &str, s2: &str) -> String {
-    let mut result = String::from("");
-
-    result.push_str(s1);
-    result.push_str(s2);
-
-    result
-}
