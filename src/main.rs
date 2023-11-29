42dbfac92087a21b1fe980f72514b60b682d16ea
Joao Paulo (JP) <joaobianchi@live.com>
2023-11-29 08:31:16 -0300

    :construction: module 2 - calculate section (enums & pattern matching)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 // method that concanate two string
 // @params:
 // s1 -> string slice
@@ -15,6 +17,35 @@ pub fn concatenate_strings(s1: &str, s2: &str) -> String {
     result
 }
 
+//------------------------------------------//
+// enum holding f64 type
+enum Operation {
+    Add(f64, f64),
+    Subtract(f64, f64),
+    Multiply(f64, f64),
+    Divide(f64, f64),
+    Unknown,
+}
+
+// function to calculate operation
+fn calculate(op: Operation) -> Result<f64, String> {
+    match op {
+        Operation::Add(value1, value2) => {
+            Ok(value1 + value2)
+        },
+        Operation::Subtract(value1, value2) => {
+            Ok(value1 - value2)
+        },
+        Operation::Multiply(value1, value2) => {
+            Ok(value1 * value2)
+        },
+        Operation::Divide(value1, value2) => {
+            Ok(value1 / value2)
+        },
+        Operation::Unknown => Err("Unknow operation.".to_string()),
+    }
+}
+
 fn main() {
     // mod 1 - ownership
     // initialize two string slices
@@ -23,4 +54,50 @@ fn main() {
 
     // call method to concatenate both and return the result
     println!("{}", concatenate_strings(string1, string2));
+
+    //------------------------------------------//
+    // mod 2 - calculator - vectors & hashmaps
+    // get the first number from prompt
+    println!("Input the first number: ");
+    let mut first_number = String::new();
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Failed to read line");
+
+    // get the operator from prompt
+    println!("Input the operator (+, -, /, *) ");
+    let mut operation = String::new();
+    io::stdin()
+        .read_line(&mut operation)
+        .expect("Failed to read line");
+    // parse to ref str
+    let operation: &str = &operation.clone();
+
+    // get second number from prompt
+    println!("Input the second number: ");
+    let mut second_number = String::new();
+    io::stdin()
+        .read_line(&mut second_number)
+        .expect("Failed to read line");
+
+    // parse numbers to f64
+    let first_number = first_number.trim().parse().unwrap();
+    let second_number =  second_number.trim().parse().unwrap();
+
+    // create operation instance
+    let op_instance: Operation = match operation {
+        "+\n" => Operation::Add(first_number, second_number),
+        "*\n" => Operation::Multiply(first_number, second_number),
+        "/\n" => Operation::Divide(first_number, second_number),
+        "-\n" => Operation::Subtract(first_number, second_number),
+        // return an error in case of unknown operator besides -, +, /, *
+        _ => Operation::Unknown,
+    };
+
+    // call method and get the result
+    let result = calculate(op_instance);
+
+    // print the result to the console
+    println!("The result is: {}", result.unwrap());
+    
 }
