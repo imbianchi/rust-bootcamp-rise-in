47a329a19fe987cbd4fbcc69ba306e5465df98a9
Joao Paulo (JP) <joaobianchi@live.com>
2023-12-04 18:55:40 -0300

    :construction: creating simple custom filter

--- filtering/src/main.rs
+++ filtering/src/main.rs
@@ -21,21 +21,25 @@ where
     T: PartialEq + Clone,
 {
     collection
-        .into_iter()
+        .into_iter() // consumable iteration
         .filter(|item| filter_condition.is_match(item))
-        .collect()
+        .collect() // collect and return
 }
 
 fn main() {
+    // create two test vectors int & str
     let original_collection = vec![1, 2, 3, 4, 5, 5, 5, 0, 9, 33];
     let string_collection = vec!["Rust", "is", "the", "best", "language", "!"];
 
+    // create the condition
     let filter_condition_one = FilterCondition { condition: 5 };
     let filter_condition_two = FilterCondition { condition: "Rust" };
 
+    // run tests
     let filtered_collection_one = custom_filter(original_collection, &filter_condition_one);
     let filtered_collection_two = custom_filter(string_collection, &filter_condition_two);
 
+    // print tests to the console
     println!("Filtered Collection Vec: {:?}", filtered_collection_one);
     println!("Filtered Collection Strings: {:?}", filtered_collection_two);
 }
